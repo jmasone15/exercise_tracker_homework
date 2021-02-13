570e76ce93c2180480bbb8f460a872fc53ae0b58
Jordan Masone <jordanmasone1@gmail.com>
2021-02-13 13:08:53 -0500

    Added HTML routing.

--- server.js
+++ server.js
@@ -1,10 +1,14 @@
 const express = require("express");
 const PORT = process.env.PORT || 8080;
+const logger = require("morgan");
 
 const app = express();
 
 app.use(express.urlencoded({ extended: true }));
 app.use(express.static("public"));
+
+app.use(require("./routes/htmlRoutes"));
+app.use(logger("dev"));
 
 app.listen(PORT, () => {
     console.log(`App running on http://localhost:${PORT}`);
