52306ea289b124abd4b0227854446e75ebbd905d
Jordan Masone <jordanmasone1@gmail.com>
2021-02-17 09:03:49 -0500

    Heroku deployment issues.

--- routes/apiRoutes.js
+++ routes/apiRoutes.js
@@ -4,83 +4,82 @@ const db = require("../models");
 const { mongo } = require("mongoose");
 
 // Get Routes
-// Aggregate fuction for get routes
-// router.get("/api/workouts", async (req, res) => {
-//     const workouts = await Workout.aggregate([
-//         {
-//             $addFields: {
-//                 totalDuration: { $sum: "$exercises.duration" }
-//             }
-//         }
-//     ]);
-//     console.log(workouts)
-//     res.json(workouts);
-// });
-
-router.get("/api/workouts", (req, res) => {
-    Workout.aggregate([
+router.get("/api/workouts", async (req, res) => {
+    const workouts = await Workout.aggregate([
         {
             $addFields: {
                 totalDuration: { $sum: "$exercises.duration" }
             }
         }
-    ], (err, data) => {
-        if (err) throw err;
-        res.json(data)
-    });
+    ]);
+    console.log(workouts)
+    res.json(workouts);
 });
 
-// router.get("/api/workouts/range", async (req, res) => {
-//     const workoutsRange = await Workout.aggregate([
+// router.get("/api/workouts", (req, res) => {
+//     Workout.aggregate([
 //         {
 //             $addFields: {
 //                 totalDuration: { $sum: "$exercises.duration" }
 //             }
 //         }
-//     ]).limit(7);
-//     res.json(workoutsRange);
+//     ], (err, data) => {
+//         if (err) throw err;
+//         res.json(data)
+//     });
 // });
 
-router.get("/api/workouts/range", (req, res) => {
-    Workout.aggregate([
+router.get("/api/workouts/range", async (req, res) => {
+    const workoutsRange = await Workout.aggregate([
         {
             $addFields: {
                 totalDuration: { $sum: "$exercises.duration" }
             }
         }
-    ], (err, data) => {
-        if (err) throw err;
-        res.json(data);
-    }).limit(7);
+    ]).limit(7);
+    res.json(workoutsRange);
 });
+
+// router.get("/api/workouts/range", (req, res) => {
+//     Workout.aggregate([
+//         {
+//             $addFields: {
+//                 totalDuration: { $sum: "$exercises.duration" }
+//             }
+//         }
+//     ], (err, data) => {
+//         if (err) throw err;
+//         res.json(data);
+//     }).limit(7);
+// });
 
 // Post Routes
-// router.post("/api/workouts", async (req, res) => {
-//     const newWorkout = await Workout.create({});
-//     res.json(newWorkout);
-// });
-
-router.post("/api/workouts", (req, res) => {
-    Workout.create({}, (err, data) => {
-        if (err) throw err;
-        res.json(data);
-    });
+router.post("/api/workouts", async (req, res) => {
+    const newWorkout = await Workout.create({});
+    res.json(newWorkout);
 });
+
+// router.post("/api/workouts", (req, res) => {
+//     Workout.create({}, (err, data) => {
+//         if (err) throw err;
+//         res.json(data);
+//     });
+// });
 
 // Put Routes
-// router.put("/api/workouts/:id", async (req, res) => {
-//     const updatedWorkout = await Workout.updateOne({ _id: req.params.id }, { $push: { exercises: req.body } });
-//     console.log(req.body);
-//     res.json(updatedWorkout);
-// });
-
-router.put("/api/workouts/:id", (req, res) => {
-    Workout.updateOne({ _id: req.params.id }, { $push: { exercises: req.body } },
-        (err, data) => {
-            if (err) throw err;
-            res.json(data);
-        }
-    );
+router.put("/api/workouts/:id", async (req, res) => {
+    const updatedWorkout = await Workout.updateOne({ _id: req.params.id }, { $push: { exercises: req.body } });
+    console.log(req.body);
+    res.json(updatedWorkout);
 });
+
+// router.put("/api/workouts/:id", (req, res) => {
+//     Workout.updateOne({ _id: req.params.id }, { $push: { exercises: req.body } },
+//         (err, data) => {
+//             if (err) throw err;
+//             res.json(data);
+//         }
+//     );
+// });
 
 module.exports = router;
